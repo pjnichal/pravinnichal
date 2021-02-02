14da04e023d49f8af12990d2c150f3be4eb7ad4a
pjnichal <nichal.pravin2001@gmail.com>
2021-02-02 20:31:55 +0530

    dayover

--- src/components/mycarousal/mycarousal.jsx
+++ src/components/mycarousal/mycarousal.jsx
@@ -4,7 +4,7 @@ import './mycarousalstyle.css'
 export const MyCarousal = () => {
     return (
         <>
-        <h1 className="center">Hi I am Pravin Nichal</h1>
+        <h1 className="center">Hello! I am Pravin Nichal</h1>
         <p className="center">Website Is Under Development🙂</p>
         </>
     )
